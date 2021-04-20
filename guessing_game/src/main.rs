e0ea778335a6edf8b65da294c9aafe5974d4ee42
dak2 <dawt2h@gmail.com>
2021-04-21 01:19:21 +0900

    add ordering

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,4 +1,5 @@
 use std::io;
+use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
@@ -6,14 +7,20 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1, 101);
 
-    println!("The secret number is: {}", secret_number);
+    println!("The secret number is: {}", secret_number);    //秘密の数字は次の通り: {}
 
-    println!("Please input daichikamiyama your guess.");
+    println!("Please input your guess.");
 
     let mut guess = String::new();
+    io::stdin().read_line(&mut guess).expect("Failed to read line");
 
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read line");
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
 
     println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),       //小さすぎ！
+        Ordering::Greater => println!("Too big!"),      //大きすぎ！
+        Ordering::Equal => println!("You win!"),        //やったね！
+    }
 }
