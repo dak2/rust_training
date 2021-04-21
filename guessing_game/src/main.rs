1b952fc0723782a0f9dfd7ead2310f573e6a128d
dak2 <dawt2h@gmail.com>
2021-04-21 23:50:04 +0900

    add other directory

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -3,24 +3,31 @@ use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
-    println!("Guess the number!");
+	println!("Guess the number!");
 
-    let secret_number = rand::thread_rng().gen_range(1, 101);
+	let secret_number = rand::thread_rng().gen_range(1, 101);
 
-    println!("The secret number is: {}", secret_number);    //秘密の数字は次の通り: {}
+	println!("The secret number is: {}", secret_number);    //秘密の数字は次の通り: {}
+	loop {
+		println!("Please input your guess.");
 
-    println!("Please input your guess.");
+		let mut guess = String::new();
+		io::stdin().read_line(&mut guess).expect("Failed to read line");
 
-    let mut guess = String::new();
-    io::stdin().read_line(&mut guess).expect("Failed to read line");
+		let guess: u32 = match guess.trim().parse() {
+			Ok(num) => num,
+			Err(_) => continue,
+		};
 
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+		println!("You guessed: {}", guess);
 
-    println!("You guessed: {}", guess);
-
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),       //小さすぎ！
-        Ordering::Greater => println!("Too big!"),      //大きすぎ！
-        Ordering::Equal => println!("You win!"),        //やったね！
-    }
+		match guess.cmp(&secret_number) {
+			Ordering::Less => println!("Too small!"),       //小さすぎ！
+			Ordering::Greater => println!("Too big!"),      //大きすぎ！
+			Ordering::Equal => {
+					println!("You win!");
+					break;
+			}
+		}
+	}
 }
---
+++ variables/target/debug/deps/variables.d
@@ -0,0 +1,5 @@
+/Users/daichi/dev_space/training/rust/variables/target/debug/deps/variables: src/main.rs
+
+/Users/daichi/dev_space/training/rust/variables/target/debug/deps/variables.d: src/main.rs
+
+src/main.rs:
